6a03ec7734b1f408f6a92d9cdb5a4b2f8c8b60af
Hutskakinator <toortgijs@outlook.com>
2024-09-05 20:41:02 +0200

    modified:   package-lock.json 	modified:   package.json 	modified:   src/events/CommandEvents/countingEvent.js

--- src/events/CommandEvents/countingEvent.js
+++ src/events/CommandEvents/countingEvent.js
@@ -5,6 +5,7 @@ module.exports = {
     name: Events.MessageCreate,
     async execute(message, client) {
         if (message.author.bot) return;
+        if (!message.guild) return;
 
         const countingData = await countingSchema.findOne({ Guild: message.guild.id });
         if (!countingData) return;
@@ -17,14 +18,13 @@ module.exports = {
 
         if (countingData.Count === 0) {
             if (number !== 1) {
-
                 const errorEmbed = new EmbedBuilder()
-                .setAuthor({ name: `${client.user.username}'s counting system` })
-                .setTitle('Incorrect Number Provided')
-                .setDescription('> You must type 1 before continuing onto other numbers.')
-                .setTimestamp()
-                .setFooter({ text: `Incorrect Number At` })
-                .setColor('Red');
+                    .setAuthor({ name: `${client.user.username}'s counting system` })
+                    .setTitle('Incorrect Number Provided')
+                    .setDescription('> You must type 1 before continuing onto other numbers.')
+                    .setTimestamp()
+                    .setFooter({ text: `Incorrect Number At` })
+                    .setColor('Red');
 
                 await message.channel.send({ embeds: [errorEmbed] });
                 return;
@@ -36,38 +36,37 @@ module.exports = {
             await countingData.save();
 
             const response = new EmbedBuilder()
-            .setAuthor({ name: `${client.user.username}'s counting system` })
-            .setTitle(`> **${message.author.username}** has successfully counted to \`${countingData.Count}\`.`)
-            .setColor('Green')
-            .setTimestamp();
+                .setAuthor({ name: `${client.user.username}'s counting system` })
+                .setTitle(`> **${message.author.username}** has successfully counted to \`${countingData.Count}\`.`)
+                .setColor('Green')
+                .setTimestamp();
 
             const reaction = await message.channel.send({ embeds: [response] });
             await reaction.react(client.config.countSuccessEmoji);
 
             if (countingData.Count === countingData.MaxCount) {
                 const congratulationsEmbed = new EmbedBuilder()
-                .setAuthor({ name: `${client.user.username}'s counting system` })
-                .setTitle('Congratulations!')
-                .setDescription(`> **${message.author.username}**, you have reached the goal of \`${countingData.MaxCount}\`! Well done!`)
-                .setTimestamp()
-                .setFooter({ text: `Game Complete` })
-                .setColor('Gold');
+                    .setAuthor({ name: `${client.user.username}'s counting system` })
+                    .setTitle('Congratulations!')
+                    .setDescription(`> **${message.author.username}**, you have reached the goal of \`${countingData.MaxCount}\`! Well done!`)
+                    .setTimestamp()
+                    .setFooter({ text: `Game Complete` })
+                    .setColor('Gold');
 
                 const congratsReact = await message.channel.send({ embeds: [congratulationsEmbed] });
-                congratsReact.react('🏆')
-
+                congratsReact.react('🏆');
 
                 countingData.Count = 0;
                 await countingData.save();
             }
         } else {
             const wrongNumberEmbed = new EmbedBuilder()
-            .setAuthor({ name: `${client.user.username}'s counting system` })
-            .setTitle('Wrong Number')
-            .setDescription(`> **${message.author.username}** has ruined the fun at number \`${countingData.Count}\`.`)
-            .setColor('Red')
-            .setTimestamp()
-            .setFooter({ text: `Wrong Number Provided` })
+                .setAuthor({ name: `${client.user.username}'s counting system` })
+                .setTitle('Wrong Number')
+                .setDescription(`> **${message.author.username}** has ruined the fun at number \`${countingData.Count}\`.`)
+                .setColor('Red')
+                .setTimestamp()
+                .setFooter({ text: `Wrong Number Provided` });
 
             await message.channel.send({ embeds: [wrongNumberEmbed] });
 
