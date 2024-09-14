14f20ed7021cdc12eb741b9059b3425600f76161
Hutskakinator <toortgijs@outlook.com>
2024-09-14 12:54:45 +0200

    Made the bon command more uh, real :)

--- src/triggers/bon.js
+++ src/triggers/bon.js
@@ -7,7 +7,7 @@ module.exports = {
     async execute(message, client, interaction) {
 
         if (message.author.bot) return;
-        if (message.content.includes('?bon'))  {
+        if (message.content.includes('fban'))  {
         
         const args = message.content.split(' ');
         const user = message.mentions.users.first() || client.users.cache.get(args[1]);
@@ -15,15 +15,15 @@ module.exports = {
 
         if (filter.words.includes(reason)) return message.reply({ content: `${client.config.filterMessage}`, ephemeral: true});
         
-        if (!user) return message.reply({ content: 'Please mention a user to bon.', ephemeral: true });
+        if (!user) return message.reply({ content: 'Please mention a user to ban.', ephemeral: true });
         
         const bonEmbed = new EmbedBuilder()
-        .setAuthor({ name: `${client.user.username} bon command`})
-        .setTitle(`> ${config.modEmojiHard}  Bon command ${config.arrowEmoji}`)
+        .setAuthor({ name: `${client.user.username} ban command`})
+        .setTitle(`> ${config.modEmojiHard}  Ban command ${config.arrowEmoji}`)
         .setColor(config.embedModHard)
         .addFields({ name: 'User', value: `> ${user.tag}`, inline: true })
         .addFields({ name: 'Reason', value: `> ${reason}`, inline: true })
-        .setFooter({ text: `Someone got got struck by the bon hammer` })
+        .setFooter({ text: `Someone got got struck by the ban hammer` })
         .setThumbnail(client.user.avatarURL())
         .setTimestamp()
         
