51c2fa951a7beaa01f60a6298a50257de725cb77
Hutskakinator <toortgijs@outlook.com>
2024-09-21 14:29:19 +0200

    f

--- src/commands/Community/aiChat.js
+++ src/commands/Community/aiChat.js
@@ -5,7 +5,7 @@ module.exports = {
     data: new SlashCommandBuilder()
     .setName('chat')
     .setDescription('Generate AI chat response')
-    .addStringOption(option => option.setName('prompt').setDescription('The prompt for gpt').setRequired(true))
+    .addStringOption(option => option.setName('prompt').setDescription('The prompt for GPT').setRequired(true))
     .addStringOption(option => option.setName('modal').setDescription('The modal to use')
     .addChoices(
             {name: "v3-32k", value: "v3-32k"},
@@ -15,17 +15,22 @@ module.exports = {
             {name: "check docs", value: "check docs"}).setRequired(false)),
     async execute(interaction) {
 
-    await interaction.deferReply()
-    const modal = interaction.options.getString('modal') || "turbo";
-    const prompt = interaction.options.getString('prompt');
+        await interaction.deferReply();
+        const modal = interaction.options.getString('modal') || "turbo";
+        const prompt = interaction.options.getString('prompt');
+        const userId = interaction.user.id;
 
         try {
-            const response = await ApexChat(modal, prompt);
+            const response = await ApexChat(modal, prompt, {
+                userId: userId,
+                memory: false,
+                instruction: ""
+            });
 
             await interaction.editReply({ content: `${response}` });
         } catch (error) {
-        console.error(error);
-        await interaction.editReply({ content: `\`\`An error occurred while generating your response. Please try again later.\`\``, ephemeral: true });
+            console.error(error);
+            await interaction.editReply({ content: `An error occurred while generating your response. Please try again later.`, ephemeral: true });
         }
     },
 };
